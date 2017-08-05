f99f27bada076f234aad1aa0b8ae911c0286b502
kkrobert <kkrobert@126.com>
2017-08-05 11:50:41 +0100

    Created by Coding Ground Client

---
+++ New Project-20170805/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+        double[] meanDemand = {1,10,30,93,53,98};
+        System.out.println(Arrays.toString( meanDemand));
+     }
+}
